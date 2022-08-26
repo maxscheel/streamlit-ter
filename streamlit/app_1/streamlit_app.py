f1691b96250a1720d33df5e78f42c7e00424fd2f
maxscheel <maximilianscheel@gmail.com>
2022-08-26 12:07:11 +1200

    Update streamlit_app.py

--- streamlit/app_1/streamlit_app.py
+++ streamlit/app_1/streamlit_app.py
@@ -1,7 +1,21 @@
 import streamlit as st
 
-import pip
-installed_packages = pip.get_installed_distributions()
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-     for i in installed_packages])
-st.text(installed_packages_list)
+import plotly.figure_factory as ff
+import numpy as np
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+         hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)

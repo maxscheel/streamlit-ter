c20a7f5e596963648fd1295d646b036bd123fc9d
maxscheel <maximilianscheel@gmail.com>
2022-08-26 15:05:42 +1200

    Update streamlit_app.py

--- streamlit/app_1/streamlit_app.py
+++ streamlit/app_1/streamlit_app.py
@@ -23,6 +23,7 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
 
+st.text("Hello Dave!")
 st.dataframe(df)
 
 st.table(df)

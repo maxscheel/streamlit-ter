153e96f04f10878d65f8cc29e5d7739a912f6a3c
maxscheel <maximilianscheel@gmail.com>
2022-08-26 12:03:05 +1200

    Update streamlit_app.py

--- streamlit/app_1/streamlit_app.py
+++ streamlit/app_1/streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit as st
-st.text("hello world")
+
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+     for i in installed_packages])
+st.text(installed_packages_list)
